b877a7e260644c4abf320111bbbb4dd17c1f0374
Joseph <60563147+JosephOBr@users.noreply.github.com>
2025-10-24 18:25:31 +1100

    Animation
    animation complete

--- animation.m
+++ animation.m
@@ -53,43 +53,60 @@ function[seriesData] = getSeries(seriesNames)
 end
 
 % READ DATA FROM FILE
-data = getSeries(["Time","R.ASISX","R.ASISY","R.Heel.BottomX","R.Heel.BottomY","L.Heel.BottomX","L.Heel.BottomY"]);
+% data = getSeries([
+% "R.ASISX","R.ASISY","L.ASISX","L.ASISY","R.PSISX","R.PSISY","L.PSISX","L.PSISY","R.Iliac.CrestX","R.Iliac.CrestY","L.Iliac.CrestX","L.Iliac.CrestY","R.Thigh.Top.LateralX","R.Thigh.Top.LateralY","L.Thigh.Top.LateralX","L.Thigh.Top.LateralY","R.Thigh.Bottom.LateralX","R.Thigh.Bottom.LateralY","L.Thigh.Bottom.LateralX","L.Thigh.Bottom.LateralY","R.Thigh.Top.MedialX","R.Thigh.Top.MedialY","L.Thigh.Top.MedialX","L.Thigh.Top.MedialY","R.Thigh.Bottom.MedialX","R.Thigh.Bottom.MedialY","L.Thigh.Bottom.MedialX","L.Thigh.Bottom.MedialY","R.Shank.Top.LateralX","R.Shank.Top.LateralY","L.Shank.Top.LateralX","L.Shank.Top.LateralY","R.Shank.Bottom.LateralX","R.Shank.Bottom.LateralY","L.Shank.Bottom.LateralX","L.Shank.Bottom.LateralY","R.Shank.Top.MedialX","R.Shank.Top.MedialY","L.Shank.Top.MedialX","L.Shank.Top.MedialY","R.Shank.Bottom.MedialX","R.Shank.Bottom.MedialY","L.Shank.Bottom.MedialX","L.Shank.Bottom.MedialY","R.Heel.TopX","R.Heel.TopY","L.Heel.TopX","L.Heel.TopY","R.Heel.BottomX","R.Heel.BottomY","L.Heel.BottomX","L.Heel.BottomY","R.Heel.LateralX","R.Heel.LateralY","L.Heel.LateralX","L.Heel.LateralY","R.MT1X","R.MT1Y","L.MT1X","L.MT1Y","R.MT5X","R.MT5Y","L.MT5X","L.MT5Y"
+% ]);
 % Save data to variables
-[timeData, rPelvisX,rPelvisY,rHeelX, rHeelY,lHeelX,lHeelY] = data{:};
-
 
 fig = figure('Color','w');
 ax  = axes('Parent', fig);
 hold(ax, 'on');          % IMPORTANT: prevents plot() from clearing prior graphics
-axis(ax, [1500 4000 0 2500]);
-% axis(ax, 'equal');
-% grid(ax, 'on');
-% xlabel(ax,'X'); ylabel(ax,'Y'); title(ax,'Marker Animation');
+axis(ax, [1500 3000 0 1500]);
+axis equal
 
+ax.XLimMode = 'manual';
+ax.YLimMode = 'manual';
 
+plot(0,0)
+h = plot(ax, nan, nan, 'b', 'LineWidth', 0.1, 'MarkerSize', 10);
+plots = [];
 
-%h = plot(ax, nan, nan, 'b', 'LineWidth', 0.1, 'MarkerSize', 10);
+for i = 1:2:length(data)
+        plots = [ plots, plot(ax,data{i}(n),data{i+1}(n),'rx','lineWidth',3) ];
+end
 
+% rfoot_plot = plot(ax, rHeelX(1), rHeelY(1), 'rx', 'LineWidth', 3);
+% lfoot_plot = plot(ax, lHeelX(1), lHeelY(1), 'rx', 'LineWidth', 3);
 
-rfoot_plot = plot(ax, rHeelX(1), rHeelY(1), 'rx', 'LineWidth', 3);
-lfoot_plot = plot(ax, lHeelX(1), lHeelY(1), 'rx', 'LineWidth', 3);
+% pelvis_plot = plot(ax, rPelvisX(1), rPelvisY(1), 'rx', 'MarkerSize', 8, 'LineWidth', 3);
 
-pelvis_plot = plot(ax, rPelvisX(1), rPelvisY(1), 'rx', 'MarkerSize', 8, 'LineWidth', 3);
-
-pauseTime = 0.005;
-for frame = 1:min(4500, numel(rHeelX))
-    currentX = get(h, 'XData');
-    currentY = get(h, 'YData');
-
-    newX = [currentX, rHeelX(frame)];
-    newY = [currentY, rHeelY(frame)];
+pauseTime = 0.005;    % ~200 FPS (adjust as needed)
+disp(length(plots))
+for frame = 2:4500
+    for n = 1:length(plots)
+        xData = data{2*n-1}(frame);
+        yData = data{2*n}(frame);
+        if mod(n,2)
+            color = 'red';
+        else
+            color = 'blue';
+        end
+        set(plots(n),'XData', xData, 'YData', yData,'Color',color);
+    end
+    
+    
+    % currentX = get(h, 'XData');
+    % currentY = get(h, 'YData');
+    % 
+    % newX = [currentX, rHeelX(frame)];
+    % newY = [currentY, rHeelY(frame)];
 
     %set(h, 'XData', newX, 'YData', newY);
 
-    set(rfoot_plot,'XData', rHeelX(frame),'YData', rHeelY(frame));
-    set(lfoot_plot,'XData', lHeelX(frame),'YData', lHeelY(frame));
-
-    set(pelvis_plot,'XData', rPelvisX(frame),'YData', rPelvisY(frame));
+    % set(rfoot_plot,'XData', rHeelX(frame),'YData', rHeelY(frame));
+    % set(lfoot_plot,'XData', lHeelX(frame),'YData', lHeelY(frame));
+    % 
+    % set(pelvis_plot,'XData', rPelvisX(frame),'YData', rPelvisY(frame));
 
     title(ax, sprintf('Frame %d', frame));
 
