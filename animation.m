8e3f4d773fc3e18071367ba31f39935436e98f17
Joseph <60563147+JosephOBr@users.noreply.github.com>
2025-10-30 19:05:39 +1100

    Adding final changes

--- animation.m
+++ animation.m
@@ -1,99 +1,156 @@
-function[seriesData] = getSeries(seriesNames)
-
-    %wb = waitbar(0,sprintf('Getting %s data', strjoin(string(seriesNames), ', ')));
-    fprintf('Reading %s from file \n',strjoin(string(seriesNames), ', '))
-    % Open the file and store the fileID into file1
-    IDfile1 = fopen("S2run35markers.txt",'r');
-    % get the number of series requested
+function [seriesData] = getSeries(seriesNames, filename)
+    fprintf('Reading %s from file \n', strjoin(string(seriesNames), ', '))
+    IDfile1 = fopen(filename, 'r');
     targetCount = numel(seriesNames);
-    % series data will accumilate the requested series in an array
     seriesData = cell(1, targetCount);
-    % seriesNums contains the index of each requested series
-    seriesNums = zeros(1,targetCount);
-    % store the row containing headers
-    headers = strsplit(fgetl(IDfile1),',');
-    % find the index of each series of interest and store it in seriesNums
+    seriesNums = zeros(1, targetCount);
+    headers = strsplit(fgetl(IDfile1), ',');
+
     for i = 1:targetCount
         seriesNums(i) = find(headers == seriesNames(i));
     end
-    % Read the file data line-by-line, store the requested values from each
-    % row in tempData, and then distribute it to each relevent series in
-    % seriesData
-    nextDatam = "";
 
+    nextDatam = "";
     i = 1;
     while ~feof(IDfile1)
-        % Read the row and split it by commas
-        nextRow = strsplit(fgetl(IDfile1),',');
-
+        nextRow = strsplit(fgetl(IDfile1), ',');
         for n = 1:targetCount
-            % Get the column(s) we're interested in into nextDatam(n1,
-            % n2,...)
             nextDatam = str2double(nextRow(seriesNums(n)));
-            % This line adds the nextdatam to the relevant series in the 'seriesData' array. 
-            % This line of code took way too long to figure out so please dont ask about it. 
-            seriesData(1,n) = {[seriesData{1,n} , nextDatam]};
-            
-            % only update UI every 1000 lines since it slows down the
-            % program dramatically
-            if mod(i,1000) == 0
-                % Chat-GPT 5 was used for this following line (since it is only
-                % for aesthetics I decided to not work it out myself)
-                %waitbar(i/(4500*targetCount), wb, ...
-                    %sprintf('Reading %s from file \n%d/%d lines read', strjoin(string(seriesNames), ', '), i, 4500*targetCount));
-                fprintf('%d/%d lines read \n', i, 4500*targetCount);
+            seriesData{1, n} = [seriesData{1, n}, nextDatam];
+
+            if mod(i, 1000) == 0
+                fprintf('%d lines read\n', i);
             end
-            i=i+1;
+            i = i + 1;
         end
     end
-    disp("Done");
-    % Close the file
     fclose(IDfile1);
-    %close(wb);
+    disp("Done");
 end
 
-% % READ DATA FROM FILE
-% data = getSeries([
-% "R.ASISX","R.ASISY","L.ASISX","L.ASISY","R.PSISX","R.PSISY","L.PSISX","L.PSISY","R.Iliac.CrestX","R.Iliac.CrestY","L.Iliac.CrestX","L.Iliac.CrestY","R.Thigh.Top.LateralX","R.Thigh.Top.LateralY","L.Thigh.Top.LateralX","L.Thigh.Top.LateralY","R.Thigh.Bottom.LateralX","R.Thigh.Bottom.LateralY","L.Thigh.Bottom.LateralX","L.Thigh.Bottom.LateralY","R.Thigh.Top.MedialX","R.Thigh.Top.MedialY","L.Thigh.Top.MedialX","L.Thigh.Top.MedialY","R.Thigh.Bottom.MedialX","R.Thigh.Bottom.MedialY","L.Thigh.Bottom.MedialX","L.Thigh.Bottom.MedialY","R.Shank.Top.LateralX","R.Shank.Top.LateralY","L.Shank.Top.LateralX","L.Shank.Top.LateralY","R.Shank.Bottom.LateralX","R.Shank.Bottom.LateralY","L.Shank.Bottom.LateralX","L.Shank.Bottom.LateralY","R.Shank.Top.MedialX","R.Shank.Top.MedialY","L.Shank.Top.MedialX","L.Shank.Top.MedialY","R.Shank.Bottom.MedialX","R.Shank.Bottom.MedialY","L.Shank.Bottom.MedialX","L.Shank.Bottom.MedialY","R.Heel.TopX","R.Heel.TopY","L.Heel.TopX","L.Heel.TopY","R.Heel.BottomX","R.Heel.BottomY","L.Heel.BottomX","L.Heel.BottomY","R.Heel.LateralX","R.Heel.LateralY","L.Heel.LateralX","L.Heel.LateralY","R.MT1X","R.MT1Y","L.MT1X","L.MT1Y","R.MT5X","R.MT5Y","L.MT5X","L.MT5Y"
-% ]);
-% % Save data to variables
+% Marker names
+seriesNames = [
+    "R.ASISX","R.ASISY","L.ASISX","L.ASISY","R.PSISX","R.PSISY","L.PSISX","L.PSISY", ...
+    "R.Iliac.CrestX","R.Iliac.CrestY","L.Iliac.CrestX","L.Iliac.CrestY", ...
+    "R.Thigh.Top.LateralX","R.Thigh.Top.LateralY","L.Thigh.Top.LateralX","L.Thigh.Top.LateralY", ...
+    "R.Thigh.Bottom.LateralX","R.Thigh.Bottom.LateralY","L.Thigh.Bottom.LateralX","L.Thigh.Bottom.LateralY", ...
+    "R.Thigh.Top.MedialX","R.Thigh.Top.MedialY","L.Thigh.Top.MedialX","L.Thigh.Top.MedialY", ...
+    "R.Thigh.Bottom.MedialX","R.Thigh.Bottom.MedialY","L.Thigh.Bottom.MedialX","L.Thigh.Bottom.MedialY", ...
+    "R.Shank.Top.LateralX","R.Shank.Top.LateralY","L.Shank.Top.LateralX","L.Shank.Top.LateralY", ...
+    "R.Shank.Bottom.LateralX","R.Shank.Bottom.LateralY","L.Shank.Bottom.LateralX","L.Shank.Bottom.LateralY", ...
+    "R.Shank.Top.MedialX","R.Shank.Top.MedialY","L.Shank.Top.MedialX","L.Shank.Top.MedialY", ...
+    "R.Shank.Bottom.MedialX","R.Shank.Bottom.MedialY","L.Shank.Bottom.MedialX","L.Shank.Bottom.MedialY", ...
+    "R.Heel.TopX","R.Heel.TopY","L.Heel.TopX","L.Heel.TopY","R.Heel.BottomX","R.Heel.BottomY", ...
+    "L.Heel.BottomX","L.Heel.BottomY","R.Heel.LateralX","R.Heel.LateralY","L.Heel.LateralX","L.Heel.LateralY", ...
+    "R.MT1X","R.MT1Y","L.MT1X","L.MT1Y","R.MT5X","R.MT5Y","L.MT5X","L.MT5Y"
+];
 
-fig = figure;
-ax  = axes('Parent', fig);
-hold(ax, 'on');         
-axis(ax, [1500 3000 0 1500]);
-axis equal
+function[dataSetNumber] = getDataSetNames()
+    disp("Note, please place the dataset as a folder of the name convention" + ...
+        "SN, where N is the number of the dataset. For example 'S1'")
 
-ax.XLimMode = 'manual';
-ax.YLimMode = 'manual';
+    disp("The appropriate folder structure is:" + newline + ...
+         "├── index.m" + newline + ...
+         "├── SN/" + newline + ...
+         "    ├── SNrun25markers.txt" + newline + ...
+         "    ├── SNrun35markers.txt" + newline + ...
+         "    ├── SNrun45markers.txt");
 
-plot(0,0)
-h = plot(ax, nan, nan, 'b', 'LineWidth', 0.1, 'MarkerSize', 10);
-plots = [];
-
-for i = 1:2:length(data)
-    plots = [ plots, plot(ax,data{i}(1),data{i+1}(1),'rx','lineWidth',3) ];
+    dataSetNumber = input("Please enter N. A number: ");  
 end
+dataSetNumber = getDataSetNames(); %Get the datasetnumber from the user
 
-pauseTime = 0.005;
-disp(length(plots))
-for frame = 2:4500
-    for n = 1:length(plots)
-        xData = data{2*n-1}(frame);
-        yData = data{2*n}(frame);
-        if mod(n,2)
-            color = 'red';
-        else
-            color = 'blue';
-        end
-        set(plots(n),'XData', xData, 'YData', yData,'Color',color);
+
+% Load data from each file
+data1 = getSeries(seriesNames, "S"+dataSetNumber+"/S"+dataSetNumber+"run25markers.txt");
+data2 = getSeries(seriesNames, "S"+dataSetNumber+"/S"+dataSetNumber+"run35markers.txt"); 
+data3 = getSeries(seriesNames, "S"+dataSetNumber+"/S"+dataSetNumber+"run45markers.txt");
+
+datasets = {data1,data2,data3};
+flightFrames = cell(1,3);
+startframes = [0,0,0];
+
+% Compute flight frames for each dataset
+for i = 1:3
+    % This was initially going to be used to measure the distance of the
+    % stride and I put it in the animation to run a visual test if it
+    % worked
+
+    currentData = datasets{i};
+    lPPelvisY = currentData{6};  % L.PSISY
+    rPPelvisY = currentData{4};  % R.PSISY
+    avgPelvis = (lPPelvisY + rPPelvisY) / 2;
+    pelvis_vel = gradient(avgPelvis);
+    pelvis_acc = gradient(pelvis_vel);
+    pelvis_jerk = gradient(pelvis_acc);
+
+    [~, flight_start] = findpeaks(-pelvis_jerk);
+    [~, flight_end] = findpeaks(pelvis_jerk);
+
+    if flight_end(1) < flight_start(1)
+        flight_end(1) = [];
     end
+    startFrames(i) = flight_start(1);
+    isFlightFrame = false(1, length(avgPelvis));
+    for k = 1:min(length(flight_start), length(flight_end))
+        isFlightFrame(flight_start(k):flight_end(k)) = true;
+    end
+    flightFrames{i} = isFlightFrame;
+end
 
-    title(ax, sprintf('Frame %d', frame));
+% Create figure and subplots
+fig = figure;
+axesArray = gobjects(1,3);
+plotsArray = cell(1,3);
 
+for i = 1:3
+    % Create the plots that need to be animated
+    axesArray(i) = subplot(1,3,i);
+    hold(axesArray(i), 'on');
+    axis(axesArray(i), [1500 3000 0 1500]);
+    axis equal
+    axesArray(i).XLimMode = 'manual';
+    axesArray(i).YLimMode = 'manual';
+
+    currentData = datasets{i};
+    plots = [];
+    for j = 1:2:length(currentData)
+        plots = [plots, plot(axesArray(i), currentData{j}(1), currentData{j+1}(1), 'rx', 'LineWidth', 3)];
+    end
+    plotsArray{i} = plots;
+end
+
+% Animate each plot
+pauseTime = 0.0005;
+for frame = 0:4500
+    for i = 1:3
+        currentData = datasets{i};
+        plots = plotsArray{i};
+        isFlightFrame = flightFrames{i};
+
+        % Update background and title
+        if isFlightFrame(startFrames(i) + frame)
+            title(axesArray(i), sprintf('Frame %d — Run %d — Flight Phase', startFrames(i) + frame, i));
+            set(axesArray(i), 'Color', [255/255 255/255 224/255]);  % light yellow
+        else
+            title(axesArray(i), sprintf('Frame %d — Run %d — Ground Phase', startFrames(i) + frame, i));
+            set(axesArray(i), 'Color', [114/255 213/255 225/255]);  % light blue
+        end
+
+        % Update marker positions
+        for n = 1:length(plots)
+            xData = currentData{2*n-1}(startFrames(i) + frame);
+            yData = currentData{2*n}(startFrames(i) + frame);
+            if mod(n,2) == 1
+                color = 'red';
+            else
+                color = 'blue';
+            end
+            set(plots(n), 'XData', xData, 'YData', yData, 'Color', color);
+        end
+    end
     drawnow;
 end
 
 disp("END");
-
 disp("*_____________________*")
--- index.m
+++ index.m
@@ -1,10 +1,10 @@
 format longG
-function[seriesData] = getSeries(seriesNames)
+function[seriesData] = getSeries(fileName,seriesNames)
 
     %wb = waitbar(0,sprintf('Getting %s data', strjoin(string(seriesNames), ', ')));
     fprintf('Reading %s from file \n',strjoin(string(seriesNames), ', '))
     % Open the file and store the fileID into file1
-    IDfile1 = fopen("S2run45markers.txt",'r');
+    IDfile1 = fopen(fileName,'r');
     % get the number of series requested
     targetCount = numel(seriesNames);
     % series data will accumilate the requested series in an array
@@ -40,6 +40,7 @@ function[seriesData] = getSeries(seriesNames)
             if mod(i,1000) == 0
                 % Chat-GPT 5 was used for this following line (since it is only
                 % for aesthetics I decided to not work it out myself)
+                % ^ DECIDED NOT TO USE SINCE IT SLOWED DOWN THE PROGRAM
                 %waitbar(i/(4500*targetCount), wb, ...
                     %sprintf('Reading %s from file \n%d/%d lines read', strjoin(string(seriesNames), ', '), i, 4500*targetCount));
                 fprintf('%d/%d lines read \n', i, 4500*targetCount);
@@ -53,84 +54,188 @@ function[seriesData] = getSeries(seriesNames)
     %close(wb);
 end
 
-function[strideDurations] = getStrideDurations(yData,timeData)
+function[strideDurations,strideLength] = getStrideDurations(yData,timeData, xData,speed)
     % mins are the INDEXES of each MINIMUM VALUE
     [~,mins] = findpeaks(-yData);
-    times0 = [timeData(mins),0];
-    times1 = [0,timeData(mins)];
 
-    strideDurations = times0 - times1;
-    strideDurations = strideDurations(1:end-1);
+    strideDurations = diff(timeData(mins));
+    xDiev = diff(xData(mins))./1000; % Get the deviation for every step
+
+    % t = 1:4500;
+    % plot(t,xData)
+    % hold on
+    % plot(mins,(xData(mins)),'o')
+
+    strideLength = (strideDurations * speed/10) + xDiev;
 end
 
-function[strideLength] = getStrideLength()
-
+function[strideDurations,nStrides,strideLengths,strideSpeeds] = calculateMetrics(rHeelY,timeData,rHeelX,speed)
+    speed = str2double(speed);
+    [strideDurations,strideLengths] = getStrideDurations(rHeelY,timeData,rHeelX,speed);
+    nStrides = 1:length(strideDurations);
+    strideSpeeds = strideLengths./strideDurations;
 end
 
-function makePlots(timeData,nStrides,strideDurations, lPelvisY, rPelvisY, ...
-    rightHeelX, rightHeelY, leftHeelX, leftHeelY)
+function[stridePercent, yNorm] = getNormalisedData(plots,colors,currentSpeed,timeData,lAPelvisY,rAPpelvisSY,lPPelvisy,rPPelvisY, rHeelY)
+    
+    [~, rMin] = findpeaks(-rHeelY);
+    
+    % average pelvis height
+    % the pelvis is tracked using 4 markers (left/right ASIS and PSIS)
+    % take the average of all 4 vertical (Y) positions to get a clean line
+    pelvisY = (lAPelvisY+rAPpelvisSY+lPPelvisy+rPPelvisY) / 4 / 1000;
+    % set up the stride time scale from 0% to 100%
+    % this makes each stride line up to the same length on the graph
+    stridePercent = linspace(0,100,101);
+    % loop through each stride (between two heel strikes)
+    for s = 1:length(rMin)-1
+        a = rMin(s);
+        b = rMin(s+1);
+        % pull out time and pelvis data for this single stride
+        tStride = timeData(a:b);
+        yStride = pelvisY(a:b);
+
+        % skip short or bad strides
+        if length(tStride) < 5
+            continue
+        end
+
+        % normalise the stride so they can all be compared easily
+        % we sample 101 evenly spaced points from start to end (0–100%)
+        strideLen = length(yStride);
+        step = floor(strideLen / 101);
+        yNorm = yStride(1:step:end);
+        
+        % if the last stride happens to have more than 101 points, trim it
+        if length(yNorm) > 101
+            yNorm = yNorm(1:101);
+        end
+        axes(plots(2,1))
+        hold on;
+        plot(stridePercent,yNorm,'Color',colors{currentSpeed});
+        title("Pelvis vertical position")
+        xlabel('Normalised Stride Time (%)')
+        ylabel("distance (m)")
+    end
+    
+end
+
+function makePlots(plots,colors,s,plotSpeed,strideDurations, strideLengths, nStrides,strideSpeeds,stridePercent, ...
+        yNorm, lHeelY,lHeelX,lToeY,lToeX,rHeelY,rHeelX,rToeY,rToeX)
     disp("Making plots")
-    figure
-    subplot(2,3,1)
+    
+    axes(plots(1,1))
+    hold on;
     plot(nStrides,strideDurations);
     title("Duration for each stride")
     xlabel("# number stride")
     ylabel("Time (s)")
+    legend(plotSpeed);
     
-    subplot(2,3,2)
-    plot(nStrides,nStrides);
+    axes(plots(1,2))
+    hold on;
+    plot([1:length(strideLengths)],strideLengths);
     title("Length of each stride")
     xlabel("# number stride")
     ylabel("Length (m)")
+    legend(plotSpeed);
     
-    subplot(2,3,3)
-    plot(nStrides,nStrides);
+    axes(plots(1,3))
+    hold on;
+    plot(nStrides,strideSpeeds)
     title("Stride speed")
     xlabel("# number stride")
     ylabel("v (m/s)")
-    
-    subplot(2,3,4)
-    plot(timeData, (lPelvisY+rPelvisY)/2); 
-    hold off;
-    title("Pelvis vertical position")
-    xlabel("Time (s)")
-    ylabel("distance (m)")
-    
-    subplot(2,3,5)
-    plot(leftHeelX,leftHeelY);
-    xlim([1400 2800])
-    ylim([0 800])
-    title("Left foot position")
-    xlabel("X data (mm)")
-    ylabel("Y data (mm)")
+    legend(plotSpeed);
 
-    
-    subplot(2,3,6)
-    plot(rightHeelX,rightHeelY);
-    xlim([1400 2800])
-    ylim([0 800])
+    axes(plots(2,1));
+    hold on
+    pVp = zeros(1,3);
+    for f = 1:3
+        pVp(f) = plot(nan, nan, colors{f}, 'LineWidth', 1.2);
+    end
+    legend(pVp, plotSpeed, 'Location', 'best')
+
+
+    axes(plots(2,2))
+    hold on;
+    plot((lHeelX+lToeX)*0.5*0.001,(lHeelY+lToeY)*0.5*0.001);
+    xlim([1.4 2.8])
+    ylim([0 0.8])
+    title("Left foot position")
+    ylabel("Foot lift (m)")
+    xlabel("Stride length (m)")
+    legend(plotSpeed);
+
+    axes(plots(2,3))
+    hold on;
+    plot((rHeelX+rToeX)*0.5*0.001,(rHeelY+rToeY)*0.5*0.001);
+    xlim([1.4 2.8])
+    ylim([0 0.8])
     title("Right foot position")
-    xlabel("X data (mm)")
-    ylabel("Y data (mm)")
+    ylabel("Foot lift (m)")
+    xlabel("Stride length (m)")
+    legend(plotSpeed);
+
+end
+
+function[plots] = createSubplots()
+    i = 1;
+    for rows = 1:2
+        for cols = 1:3
+            plots(rows,cols) = subplot(2,3,i);
+            i = i + 1;
+        end
+    end
+end
+
+function[dataSetNumber] = getDataSetNames()
+    disp("Note, please place the dataset as a folder of the name convention" + ...
+        "SN, where N is the number of the dataset. For example 'S1'")
+
+    disp("The appropriate folder structure is:" + newline + ...
+         "├── index.m" + newline + ...
+         "├── SN/" + newline + ...
+         "    ├── SNrun25markers.txt" + newline + ...
+         "    ├── SNrun35markers.txt" + newline + ...
+         "    ├── SNrun45markers.txt");
+
+    dataSetNumber = input("Please enter N. A number: ");  
 end
 
 disp("START")
 
-% READ DATA FROM FILE
-data = getSeries(["Time","R.Heel.BottomX","R.Heel.BottomY","R.Heel.BottomZ", ...
-   "L.ASISY","R.ASISY","L.Heel.BottomX","L.Heel.BottomY"]);
-% Save data to variables
-[timeData, rightHeelX, rightHeelY, rightHeelZ, lPelvisY, rPelvisY,leftHeelX,...
-    leftHeelY] = data{:};
+% Program variables
+speeds = ["25","35","45"];
+labels = ["2.5 m/s","3.5 m/s","4.5 m/s"];
+colors   = {'b','r','g'}; %blue, red and green
+data = cell(1,3);
 
-% GET STRIDE DURATIONS
-strideDurations = getStrideDurations(rightHeelY,timeData);
-nStrides = 1:length(strideDurations);
+% Creates a 3x2 matrix of plots to plot on
+plots = createSubplots();
 
-% PLOT THE STUFF
-makePlots(timeData,nStrides,strideDurations, lPelvisY, rPelvisY,rightHeelX, ...
-    rightHeelY, leftHeelX, leftHeelY);
+dataSetNumber = getDataSetNames(); %Get the datasetnumber from the user
 
+for s=1:length(speeds)
+    % Statement 2 : Read data from file
+    thisData = getSeries("S"+dataSetNumber+"/S"+dataSetNumber+"run"+speeds{s}+"markers.txt",["Time","R.Heel.BottomX",...
+       "R.Heel.BottomY", "L.ASISY","R.ASISY","L.PSISY","R.PSISY",...
+       "L.Heel.BottomX","L.Heel.BottomY","L.MT1X","L.MT1Y","R.MT1X",...
+       "R.MT1Y","L.PSISX","R.PSISX"]);
+    data(1,s) = {thisData};
 
+    % Statement 3: Assign variables to the data
+    [timeData, rHeelX, rHeelY, lAPelvisY, rAPelvisY,lPPelvisY, ...
+        rPPelvisY, lHeelX,lHeelY,lToeX,lToeY,rToeX,rToeY,lPPelvisX,rPPelvisX] = data{1,s}{:};
+
+    % Statement 4: Calculate the metrics for the graph
+    [strideDurations, nStrides, strideLengths, strideSpeeds] = calculateMetrics(rHeelY, timeData, rHeelX,speeds{s});    
+    
+    % Statement 5: Plot the graphs
+    [stridePercent, yNorm] = getNormalisedData(plots,colors,s,timeData,lAPelvisY,rAPelvisY,lPPelvisY,rPPelvisY, rHeelY);
+    makePlots(plots,colors,s,labels,strideDurations,strideLengths,nStrides, strideSpeeds, stridePercent,yNorm,lHeelY,lHeelX,lToeY,lToeX,rHeelY,rHeelX,rToeY,rToeX);
+    
+
+end
 disp("END")
 disp("*_____________________*")
